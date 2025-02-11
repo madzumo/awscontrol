406e13a57d9e4b3ac9a24a7e53e6fc2d736d85f8
Jonathan Munoz <madzumo@gmail.com>
2025-02-11 05:59:06 -0500

    update help files

--- cmd/help.go
+++ cmd/help.go
@@ -9,8 +9,8 @@ func getHelp() string {
 	token := "If you are given rotating session credentials then you will need to enter a session token here. Please note this is not needed if you use the CLI access keys assigned from a static IAM user."
 	lambda := "Lambda menu where you can List, Clone & Upgrade Lambda functions. It will upgrade to the latest version of that Runtime. Clone + Upgrade does both actions in 1 shot. Useful for cloning unsupported runtimes in AWS."
 	glue := "Glue jobs where you can List, Clone & Upgrade"
-	appendText := "This settings is to add custom text on your AWS objects that are cloned. The clone function uses the original name and appends whatever text you put in here. This is mandatory field since you can't have 2 Lambda functions with the same name."
-	replaceText := "If you want more control on where to place the Append text then use this setting. Whatever text you enter here will get replaced with the Append Text regardless of it's location in the name of the object. If the target Replace Text is not found then the default behavior of appending to the end of the name will be applied. The same default behavior will apply if you leave this field blank."
+	appendText := "Add custom text to your cloned AWS objects. The clone function uses the original name of the selected object and appends whatever text you put in here. This is a mandatory field to avoid duplicate function entries."
+	replaceText := "If you want more control on where to place the Append text then use this setting. Whatever text you enter here will get replaced with the Append Text regardless of it's location in the name of the object. If the target Replace Text is not found then the default behavior of appending to the end of the object name will be applied. The same default behavior will apply if you leave this field blank."
 
 	finalX := lipgloss.NewStyle().Foreground(lipgloss.Color("170")).Bold(true).Render(head) + "\n\n" +
 		lipgloss.NewStyle().Foreground(lipgloss.Color("112")).Bold(true).Render("Key/Secret: ") + keySecret + "\n\n" +
